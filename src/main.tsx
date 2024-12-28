151eff72fe1e5d06e4e797746f28045b82f6b375
ImanArs <imanbaevemaill.ru@gmail.com>
2024-12-28 19:12:11 +0600

    3

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import WebApp from '@twa-dev/sdk';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import WebApp from "@twa-dev/sdk";
 
 WebApp.ready();
-WebApp.setHeaderColor('bg_color');
-WebApp.setBackgroundColor('#000');
-const theme = WebApp.themeParams;
-console.log('Текущая тема:', theme);
 
-// 8142280919:AAH21nUNSSdMTISMMI97yjOM48nfrc1Rc8w
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
